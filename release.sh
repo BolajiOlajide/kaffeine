51b0df82d8bf0eec7037b293ac526aad6612ce3c
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-09-05 01:40:50 +0100

    add release tooling

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# reference: https://sourcegraph.com/github.com/sourcegraph/src-cli@main/-/blob/release.sh
+
+set -euf -o pipefail
+
+read -p "What version do you want to release? " VERSION
+
+if ! echo "$VERSION" | grep -Eq '^[0-9]+\.[0-9]+\.[0-9]+(-rc\.[0-9]+)?$'; then
+    echo -e "\033[31m\$VERSION is not in MAJOR.MINOR.PATCH format\033[0m"
+  exit 1
+fi
+
+# Create a new tag and push it, this will trigger the goreleaser workflow in .github/workflows/goreleaser.yml
+git tag "${VERSION}" -a -m "release v${VERSION}"
+# We use `--atomic` so that we push the tag and the commit if the commit was or wasn't pushed before
+git push --atomic origin main "${VERSION}"
